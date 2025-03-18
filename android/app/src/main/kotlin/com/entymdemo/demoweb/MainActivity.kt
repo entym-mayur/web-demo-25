c53bb159bad5a4647371dc7e49a4d725ed1017c9
Mayur Viras <>
2025-03-18 18:25:42 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/entymdemo/demoweb/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.entymdemo.demoweb
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:demoweb/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main()  async{
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Web Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
+
